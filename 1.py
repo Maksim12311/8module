76246e180b51794f5f4cd39e7e08453d6b96ff0d
Maksim12311 <max.brus.2016@gmail.com>
2024-03-15 17:32:06 +0300

    Second task

--- 1.py
+++ 1.py
@@ -1,27 +1,30 @@
 import sqlite3
 from datetime import datetime
 
-conn = sqlite3.connect('db4.sqlite')
+conn = sqlite3.connect('db5.sqlite')
 cursor = conn.cursor()
 
 
 cursor.execute("CREATE TABLE Students (id int, firstname Varchar(32), lastname Varchar(32), age int, city Varchar(32))")
 
-cursor.executemany("INSERT INTO Students VALUES (?, ?, ?, ?, ?)", [(1, 'Max', 'Hax', 30, 'Moscow'), (2, 'Victor', 'Nyx', 25, 'Kaliningrad'),(3, 'Igor', 'Lux', 20, 'Saint-Peterburg')])
+cursor.executemany("INSERT INTO Students VALUES (?, ?, ?, ?, ?)", [(1, 'Max', 'Brooks', 24, 'Spb'), (2, 'John', 'Stones', 15, 'Spb'), (3, 'Andy', 'Wings', 45, 'Manhester'), (4, 'Kate', 'Brooks', 34, 'Spb')])
 
 
-cursor.execute("CREATE TABLE Courses (id int, name Varchar(32), time_start timestamp, time_end timestamp)")
+cursor.execute("CREATE TABLE Courses (id int, name Varchar(32), date_start date, date_end date)")
 
 cursor.execute("CREATE TABLE Student_courses (student_id int,course_id int)")
 
 
-cursor.executemany("INSERT INTO Courses VALUES (?, ?, ?, ?)", [(11, 'Math', datetime.today(), datetime.now()), (12, 'IT', datetime.today(), datetime.now()),(13, 'PE', datetime.today(), datetime.now())])
+cursor.executemany("INSERT INTO Courses VALUES (?, ?, ?, ?)", [(1, 'python', '2021-07-21', '2021-08-21'), (2, 'java', '2021-07-13', '2021-08-16')])
 
 
-cursor.executemany("INSERT INTO Student_courses VALUES (?, ?)", [(1, 11), (2, 12), (3, 13)])
+cursor.executemany("INSERT INTO Student_courses VALUES (?, ?)", [(1, 1), (2, 1), (3, 1), (4, 2)])
 conn.commit()
 
-cursor.execute("SELECT * FROM Student_courses WHERE student_id = 1 AND course_id = 11")
+cursor.execute("SELECT * FROM Students WHERE age > 30")
 print(cursor.fetchall())
-
+cursor.execute("SELECT Students.* FROM Students JOIN Student_courses ON Students.id = Student_courses.student_id JOIN Courses ON Student_courses.course_id = Courses.id WHERE Courses.name = 'python'")
+print(cursor.fetchall())
+cursor.execute("SELECT Students.* FROM Students JOIN Student_courses ON Students.id = Student_courses.student_id JOIN Courses ON Student_courses.course_id = Courses.id WHERE Courses.name = 'python' AND Students.city = 'Spb'")
+print(cursor.fetchall())
 conn.close()
